7ea2dcad51eebf4dd2a9ee19ccaf04e53858e87e
jessylinden <jessilindenschmid@gmail.com>
2020-11-08 11:51:06 +0930

    ,

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const ctx = canvas.getContext('2d');
 
 ctx.fillStyle = 'green';
 ctx.fillRect(10, 10, 150, 100);
+
+function startup() {
+    var el = document.getElementById("canvas");
+    el.addEventListener("touchstart", handleStart, false);
+    el.addEventListener("touchend", handleEnd, false);
+    el.addEventListener("touchcancel", handleCancel, false);
+    el.addEventListener("touchmove", handleMove, false);
+  }
+  
+  document.addEventListener("DOMContentLoaded", startup);
