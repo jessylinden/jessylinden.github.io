936882bfd0278b3f1d5a5737e8c8a8c544c53b1e
jessylinden <jessilindenschmid@gmail.com>
2020-11-08 11:30:05 +0930

    .

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 150, 100);
+var ball = {
+  x: 100,
+  y: 100,
+  radius: 25,
+  color: 'blue',
+  draw: function() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+};
+
+ball.draw();
