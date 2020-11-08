99b73f478622491421590eb2c6abeb5b03559e9f
jessylinden <jessilindenschmid@gmail.com>
2020-11-08 11:55:30 +0930

    .

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function startup() {
   }
   
   document.addEventListener("DOMContentLoaded", startup);
+
+  var ongoingTouches = [];
+
+  function handleStart(evt) {
+    evt.preventDefault();
+    console.log("touchstart.");
+    var el = document.getElementById("canvas.2");
+    var ctx = el.getContext("2d");
+    var touches = evt.changedTouches;
+          
+    for (var i = 0; i < touches.length; i++) {
+      console.log("touchstart:" + i + "...");
+      ongoingTouches.push(copyTouch(touches[i]));
+      var color = colorForTouch(touches[i]);
+      ctx.beginPath();
+      ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false);  // a circle at the start
+      ctx.fillStyle = color;
+      ctx.fill();
+      console.log("touchstart:" + i + ".");
+    }
+  }
