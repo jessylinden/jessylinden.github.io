1d2df86a5d5a244067f1c2fe669a9a61d7161d78
jessylinden <jessilindenschmid@gmail.com>
2020-11-21 14:25:47 +0930

    .

--- game.js
+++ game.js
@@ -3,6 +3,45 @@ if (Blocks === undefined) {
     Blocks = {};
 }
 
-var game = BLOCKS.eventDispatcher(),
-var listener, dispatcher;
-dispatcher = BLOCKS.eventListener();listener.addEventListener('fire', eventFired);dispatcher.dispatchEvent('fire');
+
+var Blocks
+if ('Blocks' === undefined){
+    Blocks = {};
+}
+
+Blocks.game = function (spec,element) {
+
+    "use script";
+
+    var game = BLOCKS.eventDispatcher(),
+    clock = BLOCKS.clock(),
+    gameContainer,
+    gameCanvas,
+    interactionContainer,
+    paused = false,
+    virtualKeyboard,
+    motors = [],
+    tickers = [],
+    debugPressTimeout,
+    lastUpdateTime,
+    remainingUpdate,
+    minWidth,
+    minHeight,
+    maxHeight,
+    maxWidth,
+    scaleLandscape,
+    scalePortrait,
+    debugLayer,
+    gameTappedOnce,
+    loaded,
+    tickerIndex = 0,
+    layerIndex = 0,
+    maxLayers,
+    prepared,
+    wasMutedWhenPaused,
+    loadStarted,
+
+    handleTickers = function () {
+        
+    }
+}
