1ed3bca02250bd2676a3ca1498ab55cffdb7b484
jessylinden <jessilindenschmid@gmail.com>
2020-11-21 14:43:22 +0930

    .

--- introScreen.js
+++ introScreen.js
@@ -1,12 +1,4 @@
-var spec = {
-    bg: {
-        src: 'background.jpg'
-    }    
-};
-game.addEventlistener('loaded', function () {
-    game.introScreen = BLOCKS.introScreen(spec, game);
-});
-game.load()
+
 
 (function(){
     "use strict";
