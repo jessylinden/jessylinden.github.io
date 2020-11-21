0f9dabbd69c1fded2822072be258fe7d55fd821b
jessylinden <jessilindenschmid@gmail.com>
2020-11-21 15:31:30 +0930

    0

--- introScreen.js
+++ introScreen.js
@@ -1,7 +1,12 @@
 
 (function(){
     "use strict";
-
+    
+    if (true) {
+        statement_1;
+      } else {
+        statement_2;
+      }
     function greetMe(yourName) {
       alert('Hello ' + yourName);
     }
