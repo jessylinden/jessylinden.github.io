e0f4d91f76832a399eed7afd6397b1f7fedaf701
jessylinden <jessilindenschmid@gmail.com>
2020-11-21 13:29:46 +0930

    ,

--- game.js
+++ game.js
@@ -2,3 +2,7 @@ var Blocks;
 if (Blocks === undefined) {
     Blocks = {};
 }
+
+var game = BLOCKS.eventDispatcher(),
+var listener, dispatcher;
+dispatcher = BLOCKS.eventListener();listener.addEventListener('fire', eventFired);dispatcher.dispatchEvent('fire');
