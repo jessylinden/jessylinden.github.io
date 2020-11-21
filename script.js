693f920e280940c1cebe55e7e3748a0abc79d7fd
jessylinden <jessilindenschmid@gmail.com>
2020-11-21 13:06:03 +0930

    .

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 150, 100);
 
+function draw() {
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+  
+      ctx.fillRect(25, 25, 100, 100);
+      ctx.clearRect(45, 45, 60, 60);
+      ctx.strokeRect(50, 50, 50, 50);
+    }
+  }
