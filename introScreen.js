4965c7b3b885955bc2f22cf9b36f597180acafcb
jessylinden <jessilindenschmid@gmail.com>
2020-11-21 14:42:08 +0930

    ,

--- introScreen.js
+++ introScreen.js
@@ -7,3 +7,15 @@ game.addEventlistener('loaded', function () {
     game.introScreen = BLOCKS.introScreen(spec, game);
 });
 game.load()
+
+(function(){
+    "use strict";
+    /* start */
+
+    function greetMe(yourName) {
+      alert('Hello ' + yourName);
+    }
+    
+    greetMe('World');
+    /*end*/ 
+  })();
