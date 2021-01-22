731e67ad94882b9001f8eda87711424590516413
jessylinden <jessilindenschmid@gmail.com>
2021-01-22 11:14:33 +0930

    square

--- scriot2.js
+++ scriot2.js
@@ -1,2 +1,8 @@
 const canvas = document.getElementById('tester');
 const ctx = canvas.getContext('2d');
+
+var square = new Image()
+
+function init() {
+    square.scr = 'http://www.pngall.com/wp-content/uploads/5/Square-Shape-PNG-Image.png';
+}
